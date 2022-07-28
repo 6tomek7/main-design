5fd05764f2d8e0484bf314883256ba3952b7ebcd
Tomasz Pasternak <tomasz.pasternak@euvic.pl>
2022-07-28 12:29:11 +0200

    refactoring app-module and app-routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,17 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { CartComponent } from './cart/cart.component';
 import { CartService } from './cart.service';
-import { ShippingComponent } from './shipping/shipping.component';
 import { ConfigComponent } from './config/config.component';
+import { AppRoutingModule, RoutingComponents } from './app-routing.module';
 
 
 @NgModule({
@@ -21,30 +17,16 @@ import { ConfigComponent } from './config/config.component';
     BrowserModule,
     ReactiveFormsModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent },
-      { path: 'cart', component: CartComponent },
-      { path: 'shipping', component: ShippingComponent },
-    ]),
+    AppRoutingModule
   ],
 
   declarations: [
     AppComponent,
     TopBarComponent,
-    ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent,
-    CartComponent,
-    ShippingComponent,
     ConfigComponent,
+    RoutingComponents
   ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
-
-/*
-Copyright Google LLC. All Rights Reserved.
-Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
-*/
